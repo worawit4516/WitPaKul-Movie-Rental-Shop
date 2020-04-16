47b597f756d5d58a08d4e979d9ca7eee0d9d2383
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2020-04-16 11:15:33 +0700

    [REMOVE]
    เคลียร์เรโปร

--- 62130500023_62130500023_MIDLAB/StudentSimpleLibraryProject/src/lib/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package lib.service;
-import lib.item.Item;
-
-public interface MemberService {
-    public abstract ItemLending checkoutItem(MemberAccount member, Item borrowBook);
-    public abstract int checkForFine(MemberAccount member, ItemLending borrowItem);
-    public abstract boolean returnItem(MemberAccount member, ItemLending returnItem);
-    public abstract ItemLending[] getMyLendingList(MemberAccount member);
-}
